7e3deaa461e029c3ac92103a4d2562debd7457c7
memosk8 <email@email.com>
2021-02-14 19:55:41 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+#tasks = ("check email", "do laundry", "call bernie")
+
+@app.route("/new/")
+def new():
+    return render_template('new.html') 
+
+@app.route("/")
+def tasks( tasks = ("check email", "do laundry", "call bernie") ):
+    return render_template('tasks.html', tasks=tasks)  
+
+ 
